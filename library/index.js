055427d1ed27ec0e0d676477eeef5475f843bd4a
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-09-10 16:17:01 +0200

    feat: added disabled arrows in edge position, refactor carousel code

--- library/index.js
+++ library/index.js
@@ -1,83 +1,27 @@
-console.log(`
-1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part2.md
-\n
-2. Deploy: https://rolling-scopes-school.github.io/marblehands-JSFEPRESCHOOL2023Q2/library/
-\n
-3. Done 13.08.2023 / deadline 14.08.2023
-\n
-4. Score: 50 / 50
-\n
-Моя оценка после самопроверки: 50 баллов
-\n
-Привет, мой Ревьюер :) Если у тебя есть комментарии, любые советы или вопросы по моей работе, пожалуйста напиши мне RS дискорде: Ania @marblehands Спасибо за твою проверку <3
-\n
-Выполненные пункты:
-1) Вёрстка соответствует макету. Ширина экрана 768px +26
-2) Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12
-3) На ширине экрана 768рх реализовано адаптивное меню +12
-`);
-
-
-// // Burger Menu
-
-// const burger = document.querySelector('.burger-button');
-// const nav = document.querySelector('.nav');
-// const body = document.querySelector('.body');
-// const navLinks = document.querySelectorAll('.nav-link');
-// let burgerMenu = false;
-
-// burger.addEventListener('click', () => {
-//   burgerMenu = !burgerMenu;
-//   nav.classList.toggle('nav-visible');
-//   burger.classList.toggle('burger-visible');
-// });
-
-// navLinks.forEach (link => {
-//   link.addEventListener('click', function() {
-//   nav.classList.remove('nav-visible');
-//   burger.classList.remove('burger-visible');
-//   })
-// });
-
-// body.addEventListener('click', function(event) {
-//   if (event.target !== nav && event.target !== burger) {
-//   nav.classList.remove('nav-visible');
-//   burger.classList.remove('burger-visible');
-//   }
-// });
-
-// // Profile Menu
-
-
-//   const profileIcon = document.querySelector('.profile-link');
-//   const profileMenu = document.querySelector('.profile-menu');
-//   let menuDisplay = false;
-
-//   profileIcon.addEventListener('click', (event) => {
-//     event.stopPropagation();
-//     menuDisplay = !menuDisplay;
-//     profileMenu.classList.toggle('profile-menu-visible');
-//   });
-
-//   document.addEventListener('click', (event) => {
-//     if (menuDisplay) {
-//       const target = event.target;
-//       if (!profileMenu.contains(target) && (target !== profileIcon || burgerMenu)) {
-//         menuDisplay = false;
-//         burgerMenu = false;
-//         profileMenu.classList.remove('profile-menu-visible');
-//         nav.classList.remove('nav-visible');
-//         burger.classList.remove('burger-visible');
-//       }
-//     }
-//   });
-
-
+// console.log(`
+// 1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part2.md
+// \n
+// 2. Deploy: https://rolling-scopes-school.github.io/marblehands-JSFEPRESCHOOL2023Q2/library/
+// \n
+// 3. Done 13.08.2023 / deadline 14.08.2023
+// \n
+// 4. Score: 50 / 50
+// \n
+// Моя оценка после самопроверки: 50 баллов
+// \n
+// Привет, мой Ревьюер :) Если у тебя есть комментарии, любые советы или вопросы по моей работе, пожалуйста напиши мне RS дискорде: Ania @marblehands Спасибо за твою проверку <3
+// \n
+// Выполненные пункты:
+// 1) Вёрстка соответствует макету. Ширина экрана 768px +26
+// 2) Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12
+// 3) На ширине экрана 768рх реализовано адаптивное меню +12
+// `);
 
 const burger = document.querySelector('.burger-button');
 const nav = document.querySelector('.nav');
 const body = document.querySelector('.body');
 const navLinks = document.querySelectorAll('.nav-link');
+
 let burgerMenu = false;
 let menuDisplay = false;
 let modalReg = false;
@@ -202,90 +146,149 @@ radioBtn.forEach(radio => {
   })
 })
 
-// Carousel
+// // Carousel
 
-const list = document.querySelector('.carousel-list'); //ul с картинками
-const slides = document.querySelectorAll('.carousel-item'); //дивы с картинками
-const leftArrow = document.querySelector('.carousel-arrow-left'); //левая стрелка
-const rightArrow = document.querySelector('.carousel-arrow-right'); // правая стрелка
-const dotControls = document.querySelector('.controls'); // обертка с дивами в которых точки
-const controls = document.querySelectorAll('.control'); // дивы с точками
-const dots = document.querySelectorAll('.dot'); //точки (это спаны)
-const actControl = document.querySelector('.active-control'); //активная точка-спан
+// const list = document.querySelector('.carousel-list'); //ul с картинками
+// const slides = document.querySelectorAll('.carousel-item'); //дивы с картинками
+// const leftArrow = document.querySelector('.carousel-arrow-left'); //левая стрелка
+// const rightArrow = document.querySelector('.carousel-arrow-right'); // правая стрелка
+// const dotControls = document.querySelector('.controls'); // обертка с дивами в которых точки
+// const controls = document.querySelectorAll('.control'); // дивы с точками
+// const dots = document.querySelectorAll('.dot'); //точки (это спаны)
+// const actControl = document.querySelector('.active-control'); //активная точка-спан
 
 
-// это функция которая листает слайдер по клику на кнопки пагинации
-function dotClickMove () {
+// // это функция которая листает слайдер по клику на кнопки пагинации
+// function dotClickMove () {
 
-  controls.forEach((control, index)=>{ //перебираю дивы с точками
-    control.addEventListener('click', (e)=> { //слушаю клик по диву с точкой
-      let targetIndex = index;
+//   controls.forEach((control, index)=>{ //перебираю дивы с точками
+//     control.addEventListener('click', (e)=> { //слушаю клик по диву с точкой
+//       let targetIndex = index;
+//       // e.target.style.cursor = "auto";
 
-      dots.forEach((dot, index) => { // теперь перебираю точки и меняю активный класс
-        if (index === targetIndex) {
-          dot.classList.add('active-dot');
-        } else {
-          dot.classList.remove('active-dot');
-        }
-      });
+//       // let childElement = e.target.getElementsByClassName('dot');
+//       // for (let i = 0; i < childElement.length; i++) {
+//       //   childElement[i].style.cursor = "auto";
+//       // }
 
-      let distance = 475 * index; // это вычисляем отступ насколько надо подвинуть список
-      list.style.transform = `translateX(${-distance}px)` // меняю параметр translateX в css
-    })
+//       dots.forEach((dot, index) => { // теперь перебираю точки и меняю активный класс
+//         if (index === targetIndex) {
+//           dot.classList.add('active-dot');
+//         } else {
+//           dot.classList.remove('active-dot');
+//         }
+//       });
+
+//       let distance = 475 * index; // это вычисляем отступ насколько надо подвинуть список
+//       list.style.transform = `translateX(${-distance}px)` // меняю параметр translateX в css
+//     })
+//   });
+// }
+
+
+// function arrowClickMove() {
+//   leftArrow.addEventListener('click', () => {
+//     moveSlides('left');
+//   });
+
+//   rightArrow.addEventListener('click', () => {
+//     moveSlides('right');
+//   });
+// }
+
+// function moveSlides(direction) {
+//   let i = null;
+
+//   dots.forEach((dot, index) => {
+//     if (dot.classList.contains('active-dot')) {
+//       i = index;
+//     }
+//   });
+
+//   const distance = 475;
+//   if (i !== null && (direction === 'left' && i !== 0) || (direction === 'right' && i !== dots.length - 1)) {
+//     i += direction === 'left' ? -1 : 1;
+//     list.style.transform = `translateX(${-distance * i}px)`;
+//   }
+//   dots.forEach((dot, index) => {
+//   dot.classList.remove('active-dot');
+//   });
+//   dots[i].classList.add('active-dot');
+
+// }
+
+// dotClickMove();
+// arrowClickMove();
+
+//Carousel Refactoring
+
+const carouselList = document.querySelector('.carousel-list');
+const slides = document.querySelectorAll('.carousel-item');
+const leftArrow = document.querySelector('.carousel-arrow-left');
+const rightArrow = document.querySelector('.carousel-arrow-right');
+const controls = document.querySelectorAll('.control');
+const dots = document.querySelectorAll('.dot');
+let activeIndex = 0; //активный слайд
+const slideWidth = 475; //ширина шага
+
+function setActiveDot(index) {
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active-dot', i === index);
   });
 }
 
+function moveCarousel(index) {
+  const distance = -slideWidth * index;
+  carouselList.style.transform = `translateX(${distance}px)`;
+  setActiveDot(index);
+
+  // неактивные стрелки по краям
+  if (index === 0) {
+    leftArrow.style.opacity = 0.5;
+    leftArrow.style.cursor = 'default';
+  } else {
+    leftArrow.style.opacity = 1;
+    leftArrow.style.cursor = 'pointer';
+  }
+
+  if (index === slides.length - 1) {
+    rightArrow.style.opacity = 0.5;
+    rightArrow.style.cursor = 'default';
+  } else {
+    rightArrow.style.opacity = 1;
+    rightArrow.style.cursor = 'pointer';
+  }
+}
+
+function dotClickMove() {
+  controls.forEach((control, index) => {
+    control.addEventListener('click', () => {
+      if (activeIndex !== index) {
+        activeIndex = index;
+        moveCarousel(index);
+      }
+    });
+  });
+}
 
 function arrowClickMove() {
   leftArrow.addEventListener('click', () => {
-    moveSlides('left');
-  });
-
-  rightArrow.addEventListener('click', () => {
-    moveSlides('right');
-  });
-}
-
-function moveSlides(direction) {
-  let i = null;
-
-  dots.forEach((dot, index) => {
-    if (dot.classList.contains('active-dot')) {
-      i = index;
+    if (activeIndex > 0) {
+      activeIndex--;
+      moveCarousel(activeIndex);
     }
   });
 
-  const distance = 475;
-  if (i !== null && (direction === 'left' && i !== 0) || (direction === 'right' && i !== dots.length - 1)) {
-    i += direction === 'left' ? -1 : 1;
-    list.style.transform = `translateX(${-distance * i}px)`;
-  }
-  dots.forEach((dot, index) => {
-  dot.classList.remove('active-dot');
+  rightArrow.addEventListener('click', () => {
+    if (activeIndex < slides.length - 1) {
+      activeIndex++;
+      moveCarousel(activeIndex);
+    }
   });
-  dots[i].classList.add('active-dot');
-
 }
 
 dotClickMove();
 arrowClickMove();
-
-    // if (direction === 'left' && i === 0) {
-    //   leftArrow.style.opacity = 0.3;
-    //   leftArrow.style.cursor='not-allowed';
-    // }
-    // if (direction === 'right' && i === dots.length - 1) {
-    //   rightArrow.style.opacity = 0.3;
-    //   rightArrow.style.cursor='not-allowed';
-    // }
-
-
-
-//   controls.addEventListener('click', () => {
-//       console.log(checkCurrentIndex());
-//     })
-
-
 
 
 
