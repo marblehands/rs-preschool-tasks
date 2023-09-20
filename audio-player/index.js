1c4cb419946a554b072054e0c1cd38aa543f02a8
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-09-21 01:57:27 +0200

    feat: add switching playlists

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,83 +1,21 @@
-const music = {
-  retro: [
-    {
-      title: 'Безумный Мир',
-      band: 'Мираж',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/mirazh-bezumniy-mir.mp3',
-      duration: '03:40',
-    },
-    {
-      title: 'Su Di Noi',
-      band: 'Pupo',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/pupo-sudinoi.mp3',
-      duration: '03:20',
-    },
-    {
-      title: 'Storie Di Tutti I Giorni',
-      band: 'Riccardo Fogli',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
-      duration: '04:02',
-    },
-    {
-      title: 'Asphalte',
-      band: 'Alix',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/alix-asphalte.mp3',
-      duration: '02:28',
-    },
-    {
-      title: 'Asphalte',
-      band: 'Alix',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/alix-asphalte.mp3',
-      duration: '02:28',
-    },
-    {
-      title: 'Asphalte',
-      band: 'Alix',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/alix-asphalte.mp3',
-      duration: '02:28',
-    },
-  ],
-
-  punk: [
-    {
-      title: 'Безумный Мир',
-      band: 'Мираж',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/mirazh-bezumniy-mir.mp3',
-      duration: '03:40',
-    },
-    {
-      title: 'Su Di Noi',
-      band: 'Pupo',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/pupo-sudinoi.mp3',
-      duration: '03:20',
-    },
-    {
-      title: 'Storie Di Tutti I Giorni',
-      band: 'Riccardo Fogli',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
-      duration: '04:02',
-    },
-    {
-      title: 'Asphalte',
-      band: 'Alix',
-      cover: 'assets/img/cover.jpg',
-      url: 'assets/audio/retro/alix-asphalte.mp3',
-      duration: '02:28',
-    },
-  ]
-}
+import music from './playlists.js'
 
 let isPlay = false
 const soundControl = document.querySelector('.control-sound')
+const pills = Array.from(document.getElementsByName('genre'))
+const playlistWrapper = document.querySelector('.playlist-wrapper')
+
+soundControl.addEventListener('click', function () {
+  soundControlToggle ()
+})
+
+pills.forEach((pill) => {
+  pill.addEventListener('click', function () {
+    playlistWrapper.innerHTML = '';
+    getCurrentPlaylist ()
+    loadPlaylist ()
+  })
+})
 
 // Main Sound Control Toggle
 function soundControlToggle () {
@@ -89,16 +27,9 @@ function soundControlToggle () {
   }
 }
 
-soundControl.addEventListener('click', function () {
-  soundControlToggle ()
-})
-
 // Find current playlist
 function getCurrentPlaylist () {
-  const pills = document.getElementsByName('genre')
-
-  const pillsArr = Array.from(pills)
-  const pillChecked = pillsArr.filter(pill => pill.checked)
+  const pillChecked = pills.filter(pill => pill.checked)
   const playlist = pillChecked[0].id
 
   return music[playlist]
@@ -106,27 +37,28 @@ function getCurrentPlaylist () {
 
 //Paste playlist data
 function loadPlaylist () {
-  const playlistWrapper = document.querySelector('.playlist-wrapper')
   const playlist = getCurrentPlaylist ()
 
-  playlist.forEach(song => {
-    const songItemLayout = `
-        <div class="song-item-wrapper">
-          <div class="song-item-cover-wrapper">
-            <img src="${song.cover}" alt="${song.band} - ${song.title} Song Cover" class="song-item-cover-img">
-          </div>
-          <div class="song-item-info">
-            <div class="song-item-title">${song.title}</div>
-            <div class="song-item-author">${song.band}</div>
-          </div>
-          <div class="song-item-controls">
-            <span class="song-item-duration">${song.duration}</span>
-            <button class="control song-item-control"><img src="assets/svg/play-small-sign.svg" alt="" class="control-img"></button>
-          </div>
-        </div>`
+  if (playlist) {
+    playlist.forEach(song => {
+      const songItemLayout = `
+          <div class="song-item-wrapper">
+            <div class="song-item-cover-wrapper">
+              <img src="${song.cover}" alt="${song.band} - ${song.title} Song Cover" class="song-item-cover-img">
+            </div>
+            <div class="song-item-info">
+              <div class="song-item-title">${song.title}</div>
+              <div class="song-item-author">${song.band}</div>
+            </div>
+            <div class="song-item-controls">
+              <span class="song-item-duration">${song.duration}</span>
+              <button class="control song-item-control"><img src="assets/svg/play-small-sign.svg" alt="" class="control-img"></button>
+            </div>
+          </div>`
+      playlistWrapper.insertAdjacentHTML('beforeend', songItemLayout)
+    });
 
-    playlistWrapper.insertAdjacentHTML('beforeend', songItemLayout)
-  });
+  }
 }
 
 loadPlaylist ()
--- audio-player/playlists.js
+++ audio-player/playlists.js
@@ -1,4 +1,4 @@
-const music = {
+ const music = {
   retro: [
     {
       title: 'Безумный Мир',
@@ -28,6 +28,247 @@ const music = {
       url: 'assets/audio/retro/alix-asphalte.mp3',
       duration: '02:28',
     },
-  ]
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+  ],
+
+  road: [
+    {
+      title: 'Road',
+      band: 'Мираж',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/mirazh-bezumniy-mir.mp3',
+      duration: '03:40',
+    },
+    {
+      title: 'Su Di Noi',
+      band: 'Pupo',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/pupo-sudinoi.mp3',
+      duration: '03:20',
+    },
+    {
+      title: 'Storie Di Tutti I Giorni',
+      band: 'Riccardo Fogli',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
+      duration: '04:02',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+  ],
+
+  punk: [
+    {
+      title: 'Безумный Мир',
+      band: 'Мираж',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/mirazh-bezumniy-mir.mp3',
+      duration: '03:40',
+    },
+    {
+      title: 'Su Di Noi',
+      band: 'Pupo',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/pupo-sudinoi.mp3',
+      duration: '03:20',
+    },
+    {
+      title: 'Storie Di Tutti I Giorni',
+      band: 'Riccardo Fogli',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
+      duration: '04:02',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+  ],
+
+  electro: [
+    {
+      title: 'Wszystko Czego Dziś Chcę',
+      band: 'Brodka',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/electro/brodka-wszystko.mp3',
+      duration: '03:40',
+    },
+    {
+      title: 'Wrong',
+      band: 'Depeche Mode',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/pupo-sudinoi.mp3',
+      duration: '03:20',
+    },
+    {
+      title: 'Storie Di Tutti I Giorni',
+      band: 'Riccardo Fogli',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
+      duration: '04:02',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+  ],
+
+  classic: [
+    {
+      title: 'Classic',
+      band: 'Мираж',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/mirazh-bezumniy-mir.mp3',
+      duration: '03:40',
+    },
+    {
+      title: 'Su Di Noi',
+      band: 'Pupo',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/pupo-sudinoi.mp3',
+      duration: '03:20',
+    },
+    {
+      title: 'Storie Di Tutti I Giorni',
+      band: 'Riccardo Fogli',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
+      duration: '04:02',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+  ],
+
+  beauty: [
+    {
+      title: 'Чувствую',
+      band: 'Вера Сухомлин',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/vera-suhomlin-chuvstvuyu.mp3',
+      duration: '03:40',
+    },
+    {
+      title: 'Su Di Noi',
+      band: 'Pupo',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/pupo-sudinoi.mp3',
+      duration: '03:20',
+    },
+    {
+      title: 'Storie Di Tutti I Giorni',
+      band: 'Riccardo Fogli',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/riccardo-fogli-storie-di-tutti-i-giorni.mp3',
+      duration: '04:02',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+    {
+      title: 'Asphalte',
+      band: 'Alix',
+      cover: 'assets/img/cover.jpg',
+      url: 'assets/audio/retro/alix-asphalte.mp3',
+      duration: '02:28',
+    },
+  ],
+
+
 }
 export default music
