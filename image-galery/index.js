42418ae92e87a865194ce72e9fb51eab02474885
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-09-27 18:53:15 +0200

    feat: get static data from api and load to image grid

---
+++ image-galery/index.js
@@ -0,0 +1,37 @@
+// const url = 'https://api.unsplash.com/photos/random?query=nature&client_id=3On3S9wWwINjjBODC1Of5GFsFbbpsj-1MGDaW_7SSG4'
+const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=ab9b2b1f314bb455de8266b2ece22d91&tags=spring,nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1'
+const container = document.querySelector('.container')
+
+async function getData (url) {
+  const result = await fetch(url)
+  const data = await result.json()
+  return data.photos.photo
+}
+
+async function getImages () {
+  try {
+    const data = await getData (url)
+    let fragment = new DocumentFragment ()
+
+    for (let i = 0; i < data.length; i++) {
+      let div = document.createElement('div')
+      const imgUrl = data[i].url_m
+      // console.log(imgUrl)
+      div.className = 'photo-item'
+      div.style.background = `url('${imgUrl}')`
+      fragment.append(div)
+    }
+    container.append(fragment)
+  } catch (error) {
+    console.error('Error getting or displaying images:', error)
+  }
+}
+
+getImages ()
+
+
+
+
+
+
+
