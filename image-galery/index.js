a4bbc04872b854b447c5afb6a91f5cf2356cc8e7
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-09-28 02:13:00 +0200

    feat: add api and input search

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,6 +1,24 @@
 // const url = 'https://api.unsplash.com/photos/random?query=nature&client_id=3On3S9wWwINjjBODC1Of5GFsFbbpsj-1MGDaW_7SSG4'
-const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=ab9b2b1f314bb455de8266b2ece22d91&tags=spring,nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1'
+
 const container = document.querySelector('.container')
+const searchInput = document.getElementById('searchTag')
+let request = 'nature'
+let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=ab9b2b1f314bb455de8266b2ece22d91&tags=${request}&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`
+
+// searchInput.addEventListener('input', ()=>{
+  
+//   console.log(url)
+// })
+
+searchInput.addEventListener('keyup', (event)=>{
+  if ( event.code === 'Enter') {
+    event.preventDefault()
+    request = searchInput.value
+    container.innerHTML = ''
+    url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=ab9b2b1f314bb455de8266b2ece22d91&tags=${request}&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`
+    getImages ()
+  }
+})
 
 async function getData (url) {
   const result = await fetch(url)
